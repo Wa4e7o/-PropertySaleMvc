b7d7268ca0c885303fbad9b1f0fb9c468e9c1ee8
Wa4e7o <70140902+Wa4e7o@users.noreply.github.com>
2022-05-17 00:43:11 +0300

    Add some Models

---
+++ PropertySalesSystem/Data/Model/Dealer.cs
@@ -0,0 +1,17 @@
+﻿namespace PropertySalesSystem.Data.Model
+{
+    public class Dealer
+    {
+        public int Id { get; init; }
+
+        public string Name { get; set; }
+
+        public string Img { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string TypeOfSeller { get; set; }    
+
+        public List<Property> Properties { get; set; } = new List<Property>();
+    }
+}
---
+++ PropertySalesSystem/Data/Model/Property.cs
@@ -0,0 +1,24 @@
+﻿namespace PropertySalesSystem.Data.Model
+{
+    public class Property
+    {
+        public int Id { get; init; }
+
+        public string Title { get; set; }
+
+        public string Area { get; set; }
+
+        public string Location { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public double Price { get; set; }
+
+        public int DealerId {get; set;}
+
+        public Dealer Dealer { get; set; }  
+
+    }
+}
